d9a743d56f14ec34b8bfad8314691f97a4f6cc18
salisenad <senad.sali3@gmail.com>
2020-01-23 19:57:23 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:
+        {
+            apiKey: 'AIzaSyCbjqST8OwKLkzYnL9CotI0Z3Mbjnq--Us',
+            authDomain: 'gkzf-c877e.firebaseapp.com',
+            databaseURL: 'https://gkzf-c877e.firebaseio.com',
+            projectId: 'gkzf-c877e',
+            storageBucket: 'gkzf-c877e.appspot.com',
+            messagingSenderId: '775032230269'
+        }
 };
